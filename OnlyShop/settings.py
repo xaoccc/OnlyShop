63e63ba671dc9ad137c24eef59245115ffcc5fdf
xaoccc <114498517+xaoccc@users.noreply.github.com>
2024-03-27 22:14:31 +0200

    hide credentials

--- OnlyShop/settings.py
+++ OnlyShop/settings.py
@@ -183,7 +183,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'xaocccc@gmail.com'
-EMAIL_HOST_PASSWORD = 'Vitosha0703'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 
