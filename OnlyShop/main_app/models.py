a729a508bf2da7d2b4c618b5135e83224cdeeff8
xaoccc <114498517+xaoccc@users.noreply.github.com>
2024-04-14 22:42:27 +0300

    update

--- OnlyShop/main_app/models.py
+++ OnlyShop/main_app/models.py
@@ -3,7 +3,6 @@ from django.core.validators import MinValueValidator
 from django.db import models
 from django.conf import settings
 
-# Create your models here.
 User_Model = get_user_model()
 class Item(models.Model):
     ITEM_TYPES = (
