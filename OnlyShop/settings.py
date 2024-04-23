39141e6731c8d566aae8d017d5f8f44b4ac1afa9
xaoccc <xaocccc@gmail.com>
2024-04-23 12:39:24 +0300

    render deploy settings

--- OnlyShop/settings.py
+++ OnlyShop/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
